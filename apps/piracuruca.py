9b5d880a3b9c0c00210cf18c89be1c81c752b1d5
studio.alx <103322015+studioalx@users.noreply.github.com>
2024-04-30 18:42:19 -0300

    Update piracuruca.py

--- apps/piracuruca.py
+++ apps/piracuruca.py
@@ -8,5 +8,9 @@ def app():
     
 
     m = leafmap.Map(locate_control=True,location=[-3.935,-41.710], zoom_start=14, tiles="OpenStreetMap")
+
+    bairros = giseo.GeoDataFrame("https://geojson.io/api/geojson/5e8f131f7a73104a9c3e8f12/geojson")
+
+
     m.add_basemap("ROADMAP")
     m.to_streamlit(height=800)
