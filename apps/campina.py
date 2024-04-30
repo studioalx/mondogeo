f0fabf1a0b9619b29e336f15dc8a4cbeede4fe1e
studio.alx <103322015+studioalx@users.noreply.github.com>
2024-04-30 11:47:17 -0300

    Update and rename map-lions.py to campina.py

---
+++ apps/campina.py
@@ -0,0 +1,12 @@
+import streamlit as st
+import leafmap.foliumap as leafmap
+
+
+def app():
+    st.title("MAPA PIRACURUCA - VERSÃO DE TESTE")
+
+    
+
+    m = leafmap.Map(locate_control=True,location=[-3.935,-41.710], zoom_start=14, tiles="OpenStreetMap")
+    m.add_basemap("ROADMAP")
+    m.to_streamlit(height=800)
