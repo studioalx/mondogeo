99ccdfca4b43ceeb2db38fae480a9ddeafb818cd
studio.alx <103322015+studioalx@users.noreply.github.com>
2024-04-30 18:39:53 -0300

    Update piracuruca.py

--- apps/piracuruca.py
+++ apps/piracuruca.py
@@ -1,12 +1,27 @@
 import streamlit as st
+import leafmap.foliumap as leafmap
+#**********************************************************************
+"""
+import streamlit as st
+import leafmap.foliumap as leafmap
 
+
+def app():
+    st.title("MAPA PIRACURUCA - VERSÃO DE TESTE")
+
+    
+
+    m = leafmap.Map(locate_control=True,location=[-3.935,-41.710], zoom_start=14, tiles="OpenStreetMap")
+    m.add_basemap("ROADMAP")
+    m.to_streamlit(height=800)
+"""
+#**********************************************************************
+def app():
 st.title("Mapa de Piracuruca com Limites de Bairros")
 
-# Definir a centralização do mapa
-piracuruca_center = (-5.711111, -42.777778)
-
-# Criar o mapa
-map = st.map(center=piracuruca_center, zoom=10)
+   m = leafmap.Map(locate_control=True,location=[-3.935,-41.710], zoom_start=14, tiles="OpenStreetMap")
+    m.add_basemap("ROADMAP")
+    m.to_streamlit(height=800)
 
 # Carregar os dados dos bairros
 bairros = giseo.GeoDataFrame("https://geojson.io/api/geojson/5e8f131f7a73104a9c3e8f12/geojson")
