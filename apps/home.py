238ea746f3a1e24a312feae1eda2d95c0f4981f8
studio.alx <103322015+studioalx@users.noreply.github.com>
2024-04-30 11:29:13 -0300

    Update home.py

--- apps/home.py
+++ apps/home.py
@@ -7,6 +7,6 @@ def app():
 
     
 
-    m = leafmap.Map(locate_control=True,location=[-3.90,-41.745], zoom_start=13, tiles="OpenStreetMap")
+    m = leafmap.Map(locate_control=True,location=[-3.92,-41.740], zoom_start=13, tiles="OpenStreetMap")
     m.add_basemap("ROADMAP")
     m.to_streamlit(height=800)
