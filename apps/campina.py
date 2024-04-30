a18c6e404597779bb34556a57f79a115006ffadc
studio.alx <103322015+studioalx@users.noreply.github.com>
2024-04-30 12:01:06 -0300

    Update campina.py

--- apps/campina.py
+++ apps/campina.py
@@ -3,10 +3,10 @@ import leafmap.foliumap as leafmap
 
 
 def app():
-    st.title("MAPA PIRACURUCA - VERSÃO DE TESTE")
+    st.title("CAMPINA GRANDE - VERSÃO DE TESTE")
 
     
 
-    m = leafmap.Map(locate_control=True,location=[-3.935,-20.710], zoom_start=12, tiles="OpenStreetMap")
+    m = leafmap.Map(locate_control=True,location=[-3.935,-35.710], zoom_start=12, tiles="OpenStreetMap")
     m.add_basemap("ROADMAP")
     m.to_streamlit(height=800)
